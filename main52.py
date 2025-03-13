fa227c10c36536272b2ec59c40f4bdd3658e1076
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-13 11:01:29 +0000

    сделал задачу

---
+++ main52.py
@@ -0,0 +1,5 @@
+sentence = "Пример предложения с несколькими буквами с."
+char_to_find = "с"
+for char in sentence:
+    if char == char_to_find:
+        print(char)
---
+++ main53.py
@@ -0,0 +1,3 @@
+sentence = "Это пример предложения."
+for i in range(2, len(sentence), 3):
+    print(sentence[i])
