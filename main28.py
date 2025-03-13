2890bbf1da88249d661085ad5d61f791f1dab348
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-13 10:41:57 +0000

    сделал задачу

---
+++ main28.py
@@ -0,0 +1,7 @@
+word = "трос"
+sort = word[3] + word[0] + word[1] + word[2]
+rost = word[1] + word[0] + word[3] + word[2]
+tors = word[2] + word[0] + word[1] + word[3]
+print("сорт:", sort)
+print("рост:", rost)
+print("торс:", tors)
