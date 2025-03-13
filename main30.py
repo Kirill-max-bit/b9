11c507fdc5d658419c692c6d1d96abbd317814db
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-13 10:46:54 +0000

    сделал задачу

--- main30.py
+++ main30.py
@@ -1,4 +1,7 @@
 word = "апельсин"
-spaniel = word[5] + word[4] + word[0] + word[2] + word[3] + word[6] + word[7]
-+ word[1]
+spaniel = (
+    word[5] + word[4] + word[0] +
+    word[2] + word[3] + word[6] +
+    word[7] + word[1]
+)
 print("спаниель:", spaniel)
