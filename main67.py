1a9f91b28a01b4035e14d5958f8f8361eecaa1c5
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 06:23:32 +0000

    сделал задачу

---
+++ main67.py
@@ -0,0 +1,11 @@
+def count_words_967(sentence, trim_spaces=True):
+    if trim_spaces:
+        sentence = sentence.strip()
+    return len(sentence.split())  
+
+
+sentence1 = "я   иду    домой"  
+sentence2 = "   я   иду    домой   " 
+print("Случай 1:", count_words_967(sentence1, False))  
+print("Случай 2:", count_words_967(sentence2, True)) 
+ 
