f048fd002f04632c59ea2e8ba4b6ab60d9a3cabd
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 06:42:09 +0000

    сделал задачу

---
+++ main76.py
@@ -0,0 +1,5 @@
+sentence = "Елена любит есть"
+first_e = sentence.index('е') + 1
+last_e = sentence.rindex('е') + 1
+print(f"Порядковый номер первой 'е': {first_e}")
+print(f"Порядковый номер последней 'е': {last_e}")
