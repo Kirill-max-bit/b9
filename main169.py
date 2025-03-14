1cf8518c4f285003519294405fda72ec877759ce
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 09:56:51 +0000

    сделал задачу

--- main169.py
+++ main169.py
@@ -1,13 +1,10 @@
-sentence = "Дано предложение с словами, которые начинаются и оканчиваются на одну букву, например, слово око."
+sentence = "Дано предложение с словами, которые начинаются и оканчиваются на."
 
-# а) Слова, начинающиеся и оканчивающиеся на одну и ту же букву
-words_a = [word for word in sentence.split() if word[0].lower() == word[-1].lower()]
-print("а)", words_a)
+print("а)", [word for word in sentence.split()
+             if word[0].lower() == word[-1].lower()])
 
-# б) Слова, которые содержат ровно три буквы "е"
-words_b = [word for word in sentence.split() if word.lower().count('е') == 3]
-print("б)", words_b)
+print("б)", [word for word in sentence.split()
+             if word.lower().count('е') == 3])
 
-# в) Слова, которые содержат хотя бы одну букву "о"
-words_c = [word for word in sentence.split() if 'о' in word.lower()]
-print("в)", words_c)
+print("в)", [word for word in sentence.split()
+             if 'о' in word.lower()])
---
+++ main183.py
@@ -0,0 +1,16 @@
+sentence1 = "слово1 слово2 слово3 слово1"
+sentence2 = "слово2 слово4 слово5"
+
+
+from collections import Counter
+
+words1 = sentence1.split()
+words2 = sentence2.split()
+
+
+counter1 = Counter(words1)
+counter2 = Counter(words2)
+
+
+result = [word for word in set(words1 + words2) if (counter1.get(word, 0) + counter2.get(word, 0)) == 1]
+print("9.183", result)
