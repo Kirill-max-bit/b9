6f6a6ff587187cafbda0a8f05af1afa20fa25105
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 06:21:13 +0000

    сделал задачу

---
+++ main65.py
@@ -0,0 +1,24 @@
+def count_ro(sentence):
+    return sentence.count("ро")
+
+
+sentence = "просторок"
+print(count_ro(sentence))  
+
+
+def count_pair(sentence, pair):
+    return sentence.count(pair)
+
+
+sentence = "просторок"
+pair = "ст"
+print(count_pair(sentence, pair))  
+
+
+def count_substring(sentence, substring):
+    return sentence.count(substring)
+
+
+sentence = "просторок"
+substring = "оро"
+print(count_substring(sentence, substring))  
