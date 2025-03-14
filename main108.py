cfa1e029544fb2aefc4ebc187063f30bb877aced
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 07:43:55 +0000

    сделал задачу

---
+++ main108.py
@@ -0,0 +1,8 @@
+word = "глинянный"
+correct_word = word.replace("нн", "н")  # Убираем лишнюю "н"
+print(correct_word)
+
+
+word = "граффика"
+correct_word = word.replace("фф", "ф")  # Убираем лишнюю "ф"
+print(correct_word)
