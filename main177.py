105cdc393e50c0e34f95284218dd05e1498d7081
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 10:00:57 +0000

    сделал задачу

--- main177.py
+++ main177.py
@@ -1,7 +1,7 @@
+from collections import Counter
+
 sentence = "В предложении только два слова одинаковые, например, слово слово."
 
-# Найти одинаковые слова
-from collections import Counter
 word_counts = Counter(sentence.split())
 duplicate_words = [word for word, count in word_counts.items() if count == 2]
 print("9.177", duplicate_words)
