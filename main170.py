a063983e884076b4222c4dd5479b2bad5068b9c1
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 09:58:24 +0000

    сделал задачу

--- main170.py
+++ main170.py
@@ -1,5 +1,5 @@
 sentence = "Какое-нибудь слово, начинающееся на букву к, например, кот."
 
-
-word_k = next((word for word in sentence.split() if word.lower().startswith('к')), None)
+word_k = next((word for word in sentence.split()
+               if word.lower().startswith('к')), None)
 print("9.170", word_k)
