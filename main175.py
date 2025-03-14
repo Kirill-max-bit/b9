be702b6420cd2a9d560a8dce9aebd7084159e1de
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 09:59:51 +0000

    сделал задачу

--- main175.py
+++ main175.py
@@ -1,7 +1,7 @@
+from collections import Counter
+
 sentence = "Напечатать все слова, которые встречаются в нем по одному разу."
 
-
-from collections import Counter
 word_counts = Counter(sentence.split())
 unique_words = [word for word, count in word_counts.items() if count == 1]
 print("9.175", unique_words)
