c9948b1c663f8347a16f795672ced950f1dfafdd
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-14 07:56:24 +0000

    сделал задачу

---
+++ main116.py
@@ -0,0 +1,16 @@
+s = "АРГЕНТИНА МАНИТ НЕГРА"
+s = s.replace(" ", "")  # Удаляем пробелы
+is_palindrome = s == s[::-1]
+print(is_palindrome)
+
+
+s = "ПОТ КАК ПОТОП"
+s = s.replace(" ", "")  # Удаляем пробелы
+is_palindrome = s == s[::-1]
+print(is_palindrome)
+
+
+s = "А РОЗА УПАЛА НА ЛАПУ АЗОРА"
+s = s.replace(" ", "")  # Удаляем пробелы
+is_palindrome = s == s[::-1]
+print(is_palindrome)
